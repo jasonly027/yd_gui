0483091354c70242b75b252d91ac56c016be5afd
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2024-09-06 23:59:23 -0700

    Fix CI

--- cmake/SetupInstall.cmake
+++ cmake/SetupInstall.cmake
@@ -1,26 +1,25 @@
 install(TARGETS ${PROJECT_NAME}
     BUNDLE DESTINATION .
     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-    COMPONENT add_shortcut
 )
 
-# Add licenses
+# Add legal
+install(FILES
+    ${PROJECT_SOURCE_DIR}/docs/replacing_qt_lgpl3_modules.txt
+    DESTINATION share
+)
 set(LICENSE_FILES
     ${PROJECT_SOURCE_DIR}/docs/legal/Qt_License_LGPL3.txt
     ${PROJECT_SOURCE_DIR}/docs/legal/nlohmann_json_MIT.txt
-    ${PROJECT_SOURCE_DIR}/docs/legal/Noto_Sans_LICENSE.txt
-    ${PROJECT_SOURCE_DIR}/docs/legal/Typicons_LICENSE.md
+    ${PROJECT_SOURCE_DIR}/docs/legal/Noto_Sans_OFL.txt
+    ${PROJECT_SOURCE_DIR}/docs/legal/Typicons_OFL.md
 )
 install(FILES ${LICENSE_FILES}
     DESTINATION
     share/licenses
 )
 
-install(FILES
-    ${PROJECT_SOURCE_DIR}/docs/replacing_qt_lgpl3_modules.txt
-    DESTINATION share
-)
-
+# Add Qt dependencies
 qt_generate_deploy_qml_app_script(
     TARGET ${PROJECT_NAME}
     OUTPUT_SCRIPT deploy_script
@@ -29,11 +28,13 @@ qt_generate_deploy_qml_app_script(
 install(SCRIPT ${deploy_script})
 
 # Create installer
-set(CPACK_GENERATOR "IFW")
-
 if(NOT DEFINED CPACK_IFW_ROOT)
-    message(FATAL_ERROR "CPACK_IFW_ROOT must be set")
+    message(WARNING "CPACK_IFW_ROOT was not set. Skipping creating installer")
+    return()
 endif()
+
+set(CPACK_GENERATOR "IFW")
+include(CPackIFW)
 
 set(CPACK_IFW_PACKAGE_TITLE "${PROJECT_NAME} Installer")
 set(CPACK_IFW_PACKAGE_PUBLISHER "jasonly027")
@@ -45,7 +46,6 @@ set(CPACK_IFW_PACKAGE_RUN_PROGRAM "@TargetDir@/bin/${PROJECT_NAME}")
 set(CPACK_IFW_PACKAGE_MAINTENANCE_TOOL_NAME "Uninstall")
 set(CPACK_IFW_PACKAGE_CONTROL_SCRIPT ${PROJECT_SOURCE_DIR}/cmake/controller.qs)
 
-include(CPackIFW)
 cpack_add_component(add_shortcut)
 cpack_ifw_configure_component(add_shortcut
     VIRTUAL
--- tests/tst_qml.cpp
+++
@@ -1,3 +0,0 @@
-#include <QtQuickTest>
-
-QUICK_TEST_MAIN(yd_gui_qml_tests)
